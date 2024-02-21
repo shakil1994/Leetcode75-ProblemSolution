0c24c691b16f189947c4714c0763c81adb02ce04
android_shakil <shakil!@#$1234>
2024-02-21 15:17:49 +0600

    700. Search in a Binary Search Tree

---
+++ Binary_Search_Tree/700. Search in a Binary Search Tree.kt
@@ -0,0 +1,24 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        if(root == null || root.`val` == `val`){
+            return root
+        }
+
+        return if(`val` < root.`val`){
+            searchBST(root.left, `val`)
+        }
+        else{
+            searchBST(root.right, `val`)
+        }
+    }
+}
