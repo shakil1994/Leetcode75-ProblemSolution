6629a4087b9685b3ec4177b01051b603704110d5
android_shakil <shakil!@#$1234>
2024-02-28 23:19:38 +0600

    1143. Longest Common Subsequence

---
+++ DP_Multidimensional/1143. Longest Common Subsequence.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        val m = text1.length
+        val n = text2.length
+        val dp = Array(m + 1) { IntArray(n + 1) }
+
+        for (i in 1..m) {
+            for (j in 1..n) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = maxOf(dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+
+        return dp[m][n]
+    }
+}
